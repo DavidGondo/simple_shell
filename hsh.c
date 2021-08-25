ebf27d3de023367aa8c3e607a3b8819bf4723aa3
DavidGondo <d.gondo@alustudent.com>
2021-08-25 07:56:38 -0700

    Update

--- hsh.c
+++ hsh.c
@@ -6,10 +6,19 @@
 #include <sys/wait.h>
 
 /**
+*main: entry to program
+*
 *startDisplay - first prompt
-*Retun: void
+*
+*displayPrompt - prompt to display when program starts
+*
+*makeTokens - tokenixning the line
+*
+*execute -executing the program
+*
+*Return - 0 if success
+*
 */
-<<<<<<< HEAD:epo.c
 
 char *input = NULL;
 size_t capline = 0;
@@ -18,9 +27,6 @@ char *token;
 char *array[512];
 
 void startDisplay()
-=======
-void startDisplay(void)
->>>>>>> 7f410ce806a6080abf387a7ed1772b11f0fce2be:hsh.c
 {
 	printf("**************************************************************\n");
 	printf("*WELCOME TO THE COMMAND LINE INTERPETER                      *\n");
@@ -30,23 +36,13 @@ void startDisplay(void)
 	printf("**************************************************************\n");
 }
 
-/**
-*displayPrompt - prompt to display when program starts
-*Return: void
-*/
 void displayPrompt(void)
 {
-	printf("$");
+printf("$");
 }
 
-/**
-*makeTokens - tokenixning the line
-*@input: input to be token
-*Return: void
-*/
 void makeTokens(char *input)
 {
-<<<<<<< HEAD:epo.c
 i = 0;
 token = strtok(input, "\n ");
 while (token != NULL)
@@ -55,29 +51,11 @@ array[i++] = token;
 token = strtok(NULL, "\n ");
 }
 array[i] = NULL;
-=======
-	int i = 0;
-	char *array[512];
-	char *token = strtok(input, "\n ");
-
-	while (token != NULL)
-	{
-		array[i++] = token;
-		char *token = strtok(NULL, "\n ");
-	}
-
-	array[i] = NULL;
->>>>>>> 7f410ce806a6080abf387a7ed1772b11f0fce2be:hsh.c
 
 }
 
-/**
-*execute - executing the program
-*Return: void
-*/
 void execute(void)
 {
-<<<<<<< HEAD:epo.c
  int pid = fork();
  if (pid != 0)
  {
@@ -108,61 +86,12 @@ int main()
 	makeTokens(input);
 	
 	if (strcmp(array[0], "q") == 0)
-=======
-	char *array[512];
-	int pid = fork();
-
-	if (pid != 0)
 	{
-		int s;
-
-		waitpid(-1, &s, 0);
+	  printf("SYSTEM : Shell is exit\n");
+	  return 0;
 	}
-	else
-	{
-		if (execvp(array[0], array) == -1)
-		{
-			perror("Wrong command");
-			exit(errno);
-		}
-	}
-}
+	
+	execute();
 
-/**
-*main - Entry to program
-*Return: 0 on success
-*/
-int main(void)
-{
-	char *input = NULL;
-	size_t capline = 0;
-	int i;
-	char *token;
-	char *array[512];
-
-	startDisplay();
-
-	while (1)
->>>>>>> 7f410ce806a6080abf387a7ed1772b11f0fce2be:hsh.c
-	{
-		displayPrompt();
-		getline(&input, &capline, stdin);
-
-		if (strcmp(input, "\n") == 0)
-		{
-			perror("Please type in a command ");
-			continue;
-		}
-
-		makeTokens(input);
-
-		if (strcmp(array[0], "q") == 0)
-		{
-			printf("SYSTEM : Shell is exit\n");
-			return (0);
-		}
-
-		execute();
-
-	}
+      }
 }
